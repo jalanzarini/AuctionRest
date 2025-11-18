04290a72759391bf30bec88ff5ed49d67068f3f7
jalanzarini <joaoandrelanzarini@gmail.com>
2025-11-18 19:32:26 -0300

    run script (alacritty)

---
+++ backend/run_ms.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+echo "Starting APIGateway.py in the background..."
+alacritty --working-directory ~/Faculdade/SistDist/AuctionRest/backend/ -e uv run APIGateway.py &
+
+echo "Starting MSLeilao.py in the background..."
+alacritty --working-directory ~/Faculdade/SistDist/AuctionRest/backend/ -e uv run MSLeilao.py &
+
+echo "Starting MSLance.py in the background..."
+alacritty --working-directory ~/Faculdade/SistDist/AuctionRest/backend/ -e uv run MSLance.py &
+
+echo "Starting MSPagamento.py in the background..."
+alacritty --working-directory ~/Faculdade/SistDist/AuctionRest/backend/ -e uv run MSPagamento.py &
+
+echo "Starting MockPayment.py in the background..."
+alacritty --working-directory ~/Faculdade/SistDist/AuctionRest/backend/ -e uv run MockPayment.py &
+
+# Wait for all background jobs to complete before exiting the script
+#wait
+
+#echo "All background scripts finished."
