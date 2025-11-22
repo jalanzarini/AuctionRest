dc403d06756c2efb6729d8895f971e68efb9d150
Alan <alanyoshiotara@gmail.com>
2025-11-22 17:58:23 -0300

    Um nome descendente

--- frontend/src/components/MessageList.vue
+++ frontend/src/components/MessageList.vue
@@ -2,13 +2,14 @@
 import { ref } from "vue";
 const props = defineProps(["userId"]);
 const messages = ref(null);
+messages.value = [];
 let source = new EventSource(
   `http://localhost:5000/stream?channel=${props.userId}`,
 );
 source.addEventListener("message", function (event) {
   let data = JSON.parse(event.data);
   console.log(data.message);
-  messages.append(data.message);
+  messages.value.push(data.message);
 });
 </script>
 
@@ -17,8 +18,59 @@ source.addEventListener("message", function (event) {
     <v-list>
       <v-list-item v-for="(message, index) in messages">
         <v-divider v-if="index != 0"></v-divider>
-        <v-list-item-title>
-          {{ message.auction.name + " " + message.message }}
+        <v-list-item-title v-if="message.type === 'validado'">
+          {{
+            "[LANCE] ID Leilao: " +
+            message.id_leilao +
+            " - Usuário: " +
+            message.id_user +
+            " - Valor: " +
+            message.valor_lance
+          }}
+        </v-list-item-title>
+        <v-list-item-title v-else-if="message.type === 'invalidado'">
+          {{
+            "[LANCE INVALIDO] ID Leilao: " +
+            message.id_leilao +
+            " - Usuário: " +
+            message.id_user +
+            " - Valor: " +
+            message.valor_lance
+          }}
+        </v-list-item-title>
+        <v-list-item-title v-else-if="message.type === 'vencedor'">
+          {{
+            "[VENCEDOR] ID Leilao: " +
+            message.id_leilao +
+            " - Usuário: " +
+            message.id_user_vencedor +
+            " - Valor: " +
+            message.valor_vencedor
+          }}
+        </v-list-item-title>
+        <v-list-item-title v-else-if="message.type === 'link_pagamento'">
+          {{
+            "[LINK PAGAMENTO] ID Leilao: " +
+            message.id_leilao +
+            " - Link pagamento: " +
+            message.payment_link +
+            " - Valor: " +
+            message.currency +
+            " " +
+            message.amount
+          }}
+        </v-list-item-title>
+        <v-list-item-title v-else-if="message.type === 'status_pagamento'">
+          {{
+            "[STATUS PAGAMENTO] ID Leilao: " +
+            message.id_leilao +
+            " - Usuário: " +
+            message.id_user +
+            " - Valor: " +
+            message.amount +
+            " - Status: " +
+            message.status
+          }}
         </v-list-item-title>
       </v-list-item>
     </v-list>
